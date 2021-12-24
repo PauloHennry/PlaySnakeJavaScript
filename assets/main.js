a8a3f903e155cfccd45fcf852fc7559683247107
PauloHennry <Ariewl.marcelino9904@gmail.com>
2021-12-23 21:21:01 -0300

    final do jogo

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,6 @@
 
+alert("Bem-Vindo ao jogo da Cobrinha")
+
 let canvas = document.getElementById("canvas");
 let context = canvas.getContext("2d");
 let box = 32;
@@ -9,6 +11,7 @@ snake[0]={
 }
 
 let direction = "right";
+
 let food = {
     x:  Math.floor(Math.random() * 15 + 1) * box,
     y:  Math.floor(Math.random() * 15 + 1) * box
@@ -20,7 +23,7 @@ function criarBG(){
 }
 
 function criarCobrinha(){
-    for(i=0; i < snake.length; i++) {
+    for(i = 0; i < snake.length; i++){
         context.fillStyle = "black";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
@@ -41,10 +44,19 @@ function update(event){
 }
 
 function iniciarJogo(){
+
     if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
     if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
+    for(i = 1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert("GameOver !! :(");
+            alert("Obrigado por jogar - Game feito a partir do estudo de Html ,Css ,JavaScripti - Desenvolvido por Paulo Silva")
+        }
+    }
 
     criarBG();
     criarCobrinha();
@@ -56,16 +68,22 @@ function iniciarJogo(){
     if(direction == "right") snakeX += box;
     if(direction == "left") snakeX -= box;
     if(direction == "up") snakeY -= box;
-    if(direction == "down") snakeY -= box;
+    if(direction == "down") snakeY += box;
 
-    snake.pop();
+    if(snakeX != food.x || snakeY != food.y){
+        snake.pop();
+    }
+    else{
+         food.x = Math.floor(Math.random() * 15 + 1) * box;
+         food.y = Math.floor(Math.random() * 15 + 1) * box;
+    }
 
     let newHead = {
         x: snakeX,
         y: snakeY
     }
 
-    snake.unshift(iniciarJogo, 100);
+    snake.unshift(newHead);
 }
 
 let jogo = setInterval(iniciarJogo, 100);
